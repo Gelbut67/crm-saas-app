afbed13754b62642630210313509c603f10203cc
Garip YASAR <yasar@belle-etiquette.com>
2026-06-17 10:49:20 +0200

    Fix: add mobile-web-app-capable meta tag to suppress deprecation warning

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,9 @@ export const metadata: Metadata = {
   formatDetection: {
     telephone: false,
   },
+  other: {
+    'mobile-web-app-capable': 'yes',
+  },
   openGraph: {
     type: 'website',
     siteName: 'BELLE ETIQUETTE',
